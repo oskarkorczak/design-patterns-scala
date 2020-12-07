0982e6e46fd41685415924ddec9c2b95d9f9db7b
Oskar Korczak <o.korczak@gmail.com>
2020-12-07 13:04:04 +0000

    Add Typeclass inference for complex types: Tuples[T, V]

--- src/test/scala/com/korczak/patterns/typeclass/StringParserSpec.scala
+++ src/test/scala/com/korczak/patterns/typeclass/StringParserSpec.scala
@@ -12,6 +12,15 @@ final class StringParserSpec extends UnitSpec {
     implicitly[StringParser[T]].parse(s)
   }
 
+  implicit def ParseTuple[T, V](implicit p1: StringParser[T], p2: StringParser[V]) = {
+    new StringParser[(T, V)] {
+      override def parse(s: String): (T, V) = {
+        val Array(left, right) = s.split('=')
+        (p1.parse(left), p2.parse(right))
+      }
+    }
+  }
+
   behavior of "String parser"
 
   it should "parse Int" in {
@@ -33,5 +42,23 @@ final class StringParserSpec extends UnitSpec {
 
     parse[Boolean](str) shouldEqual true
     parseNeat[Boolean](str) shouldEqual true
+  }
+
+  it should "parse Tuples of [Int, Double]" in {
+    val str = "15=240.5"
+
+    parse[(Int, Double)](str) shouldEqual (15, 240.5)
+  }
+
+  it should "parse Tuples of [Int, Boolean]" in {
+    val str = "15=true"
+
+    parse[(Int, Boolean)](str) shouldEqual (15, true)
+  }
+
+  it should "parse Tuples of [Double, Boolean]" in {
+    val str = "240.5=false"
+
+    parse[(Double, Boolean)](str) shouldEqual (240.5, false)
   }
 }
